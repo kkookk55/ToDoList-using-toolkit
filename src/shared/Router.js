fbb3549089374ff504933d20b291c5ffb18b9f6f
Hyeji Kang <owlgpwl94@gmail.com>
2022-12-14 14:00:51 -0800

    Update Router.js

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,15 +3,15 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Detail from "../pages/Detail";
 import List from "../pages/List";
 import Main from "../pages/Main";
-
+import Modify from "../pages/Modify";
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="detail" element={<Detail />} />
-        <Route path="list" element={<List />} />
+        <Route path="detail/:id" element={<Detail />} />
         <Route path="list/:category" element={<List />} />
+        <Route path="modify/:id" element={<Modify />} />
       </Routes>
     </BrowserRouter>
   );
